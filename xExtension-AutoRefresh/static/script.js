b8516bf15aeffbda2aba676e5a27cbf8ce66c2a6
Charlie <Charlie@sirideain.org>
2018-09-12 09:44:18 -0400

    Add touch events for mobile, fix event binding, and replace setTimeout with setInterval.

--- xExtension-AutoRefresh/static/script.js
+++ xExtension-AutoRefresh/static/script.js
@@ -1,17 +1,10 @@
-$(document).ready(function() {
-
-     var time = new Date().getTime();
-     $(document.body).bind("mousemove keypress", function(e) {
-         time = new Date().getTime();
-     });
-
-     function refresh() {
-         if(new Date().getTime() - time >= 60000) 
-             window.location.reload(true);
-         else 
-             setTimeout(refresh, 10000);
-     }
-
-     setTimeout(refresh, 10000);
-
+var time = new Date().getTime();
+$(document).bind("touchmove mousemove keypress", function () {
+    time = new Date().getTime();
 });
+
+setInterval(function() {
+    if (new Date().getTime() - time >= 60000) {
+        window.location.reload(true);
+    }
+}, 30000);
