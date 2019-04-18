6deef35442cd5d3e79e413292a2dce2e137da49d
tofulm <tofulm@gmail.com>
2019-04-18 16:28:32 +0200

    with last freshrss, reload page don't actualize. So, I replace withOC a trigger on actualize icon

--- xExtension-AutoRefresh/static/script.js
+++ xExtension-AutoRefresh/static/script.js
@@ -1,10 +1,11 @@
 var time = new Date().getTime();
-$(document).bind("touchmove mousemove keypress", function () {
+
+$(document).on("touchmove mousemove keypress", function () {
     time = new Date().getTime();
 });
 
 setInterval(function() {
     if (new Date().getTime() - time >= 60000) {
-        window.location.reload(true);
+		$('#actualize').trigger('click');
     }
 }, 30000);
